6091df7bd78dec1a6aeeb3070efe3030b82c6464
Siri <liberatib14@gmail.com>
2025-05-26 16:38:16 -0300

    C5 Completado

--- C5 Fundamentos Javascript/ejercicios/30.js
+++ C5 Fundamentos Javascript/ejercicios/30.js
@@ -3,10 +3,12 @@ function deEuroAdolar(euro) {
   // Debes calcular el valor recibido como argumento pasándolo a dólares.
   // Suponiendo que 1 euro equivale a 1.20 dólares.
   // Retornar el valor en dolares.
-  // Por ejemplo: 
+  // Por ejemplo:
   // 1 ---> 1.20
   // 0 ---> 0
   // Tu código:
+  var dolares = euro * 1.2;
+  return dolares;
 }
 
 module.exports = deEuroAdolar;
